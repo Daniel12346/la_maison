a23c6c4c787fe5dfc1d03f1978851a147a67d5e9
Daniel12346 <danezoki2@gmail.com>
2026-03-11 21:16:16 +0100

    added dynamically fetched timeSlot choices, submit button and comments to clarify code

--- src/Form/ReservationFormType.php
+++ src/Form/ReservationFormType.php
@@ -3,10 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Reservation;
+use App\Repository\ReservationRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
@@ -17,9 +19,15 @@ use Symfonycasts\DynamicForms\DynamicFormBuilder;
 
 class ReservationFormType extends AbstractType
 {
+    protected $repository;
+    public function __construct(ReservationRepository $repository)
+    {
+        $this->repository = $repository;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder = new DynamicFormBuilder($builder);
+        //polja za partySize, date i isPrivate su uvijek prikazana
         $builder
             ->add('partySize')
             ->add('date', DateType::class, [
@@ -32,13 +40,24 @@ class ReservationFormType extends AbstractType
             ])
             ->addDependent('timeSlot', ['date', 'partySize', "isPrivate"], function (DependentField $field, ?\DateTime $date, ?int $partySize, ?bool $isPrivate) {
                 if (!$date || !$partySize) {
+                    //polje za timeSlot se prikazuje samo ako je date i partySize popunjeno
+                    //isPrivate ne mora biti popunjen jer ima default vrijednost false
                     return;
                 }
+                //ponuđene opcije za timeSlot ovise o date, partySize i isPrivate i automatski se mijenjaju kad korisnik promijeni vrijednost nekog od tih polja
+                $timeSlots = $this->repository->getAvailableTimeSlots($date, $partySize, $isPrivate);
+                $choices = [];
+                foreach ($timeSlots as $timeSlot) {
+                    $choices[$timeSlot] = new \DateTime($timeSlot);
+                }
+
                 $field->add(ChoiceType::class, [
-                    'label' => 'Time'
+                    'label' => 'Time',
+                    'choices' => $choices
                 ]);
             })
-
+            //ostala polja se prikazuju ako postoji timeSlot 
+            //nema potrebe da korisnik upisuje email, broj mobitela, ime i posebne zahtjeve dok ne otkrije postoji li slobodan timeSlot
             ->addDependent('email', ['timeSlot'],  function (DependentField $field, ?\DateTime $timeSlot) {
                 if (!$timeSlot) {
                     return;
@@ -76,11 +95,19 @@ class ReservationFormType extends AbstractType
                     ->add(TextareaType::class, [
                         'label' => 'Requests',
                     ]);
+            })
+            ->addDependent('submit', 'timeSlot', function (DependentField $field, ?\DateTime $timeSlot) {
+                if (!$timeSlot) {
+                    return;
+                }
+
+                $field->add(SubmitType::class);
             });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        //polja forma odgovaraju poljima entiteta Reservation
         $resolver->setDefaults([
             'data_class' => Reservation::class,
         ]);
