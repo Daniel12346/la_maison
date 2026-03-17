844a628b1d537dcebb171a4b09b6129e80d71346
Daniel12346 <danezoki2@gmail.com>
2026-03-17 07:44:17 +0100

    Added save() LiveAction so the form submits through the live component

--- src/Twig/Components/ReservationForm.php
+++ src/Twig/Components/ReservationForm.php
@@ -2,10 +2,14 @@
 
 namespace App\Twig\Components;
 
+use App\Entity\Reservation;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use App\Form\ReservationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
@@ -16,8 +20,29 @@ final class ReservationForm extends AbstractController
     use ComponentWithFormTrait;
     use DefaultActionTrait;
 
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+    ) {
+    }
+
     protected function instantiateForm(): FormInterface
     {
         return $this->createForm(ReservationFormType::class);
+    }
+
+    #[LiveAction]
+    public function save(): RedirectResponse
+    {
+        $this->submitForm();
+
+        /** @var Reservation $reservation */
+        $reservation = $this->getForm()->getData();
+
+        $this->entityManager->persist($reservation);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('confirmation', [
+            'referenceCode' => $reservation->getReferenceCode(),
+        ]);
     }
 }
